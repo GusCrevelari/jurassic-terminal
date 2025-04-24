b624c79905004d174b800778afbf608073d349c8
GusCrevelari <gustavocrevelari@hotmail.com>
2025-04-24 10:09:41 -0300

    General Fixes

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException { // InterruptedException for thread.sleep
         // to work.
 
-        // Simple scanner to start...
+        // Simple scanner to start... Commit Test
 
         //Scanner scanner = new Scanner(System.in); # We dont need this, we are not printing in the console.
 
